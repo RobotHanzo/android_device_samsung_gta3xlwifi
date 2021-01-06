39713f36e4aec04a9ef2ed18aa011c6bcc5b84ba
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2021-01-06 21:28:16 +0800

    Updated for OrangeFox

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,39 @@
 # limitations under the License.
 #
 
+Default_Settings() {
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/13500000.dwmmc0/by-name/recovery"
+	export FOX_REPLACE_BUSYBOX_PS="1"
+	export FOX_USE_BASH_SHELL="1"
+	export FOX_USE_LZMA_COMPRESSION="1"
+	export FOX_USE_NANO_EDITOR="1"
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+	export LC_ALL="C"
+	export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+	export OF_OTA_RES_DECRYPT="1"
+  export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+  export OF_USE_NEW_MAGISKBOOT="1"
+	export TARGET_ARCH="arm64"
+	export TW_DEFAULT_LANGUAGE="en"
+  export FOX_VERSION="R11.0"
+  export FOX_R11="1"
+	export USE_CCACHE="1"
+	export OF_FLASHLIGHT_ENABLE="0"
+	export OF_SCREEN_H="1920"
+
+  	# lzma
+  	[ "$FOX_USE_LZMA_COMPRESSION" = "1" ] && export LZMA_RAMDISK_TARGETS="recovery"
+
+  	# A/B devices
+  	#[ "$OF_AB_DEVICE" = "1" ] && export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+
+  	# magiskboot
+  	[ "$OF_USE_MAGISKBOOT_FOR_ALL_PATCHES" = "1" ] && export OF_USE_MAGISKBOOT="1"
+}
+
+
 add_lunch_combo omni_gta3xlwifi-eng
 
