9255c9ce27b471c5673139376910a06f71cab862
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2022-02-23 22:59:38 +0800

    Fixed install partition

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,6 +22,7 @@ Default_Settings() {
 	export FOX_USE_LZMA_COMPRESSION="1"
 	export FOX_USE_NANO_EDITOR="1"
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
 	export LC_ALL="C"
 	export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
 	export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
